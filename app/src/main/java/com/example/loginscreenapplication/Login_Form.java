5eb853ee12985d8832a9232eb30ecdab9bba1df6
Nathaniel Brew Mowl <nathaniel.mowl@regent.edu.gh>
2021-11-04 17:42:05 +0000

    Assignment on Login Screen Application

---
+++ app/src/main/java/com/example/loginscreenapplication/Login_Form.java
@@ -0,0 +1,23 @@
+package com.example.loginscreenapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login_Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
+        getSupportActionBar() .setTitle("Login Form");
+    }
+
+    public void btn_signupForm(View view) {
+
+        startActivity(new Intent(getApplicationContext(),Signup_Form.class));
+
+    }
+}
